ce6f93d75b745c7f0d2bfe657ac0b8089dbe100b
Gaturama <bielsobv@gmail.com>
2023-12-01 09:28:52 -0300

    react commit

--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -3,6 +3,9 @@ import TexasLogo from '../assets/images/logo.png';
 import { Link } from 'react-router-dom';
 import { Carousel } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import bannerImage from '../assets/images/banner.png';
+
+import { BsChevronLeft, BsChevronRight } from 'react-icons/bs'
 
 export default function HomeScreen() {
     return (
@@ -16,7 +19,6 @@ export default function HomeScreen() {
                         <li><Link to="/professor">Professor</Link></li>
                         <li><Link to="/aluno">Aluno</Link></li>
                         <li><Link to="/disciplina">Disciplina</Link></li>
-                        {/* <li><Link to="/lista-alunos">Lista de Alunos</Link></li> */}
                     </ul>
                 </nav>
                 <div className={styles.redes}>
@@ -26,29 +28,36 @@ export default function HomeScreen() {
                 </div>
             </header>
             <div className={styles['carousel-container']}>
-                <Carousel>
+                <Carousel
+                    nextIcon={<BsChevronRight />}
+                    prevIcon={<BsChevronLeft />}
+                >
                     <Carousel.Item>
-                        <img src='https://picsum.photos/1920/400' alt="Slide 1" />
-                        <Carousel.Caption>
-                            <h3>Slide 1</h3>
-                        </Carousel.Caption>
+                        <img src={bannerImage} alt='Slide 1' />
                     </Carousel.Item>
                     <Carousel.Item>
-                        <img src='https://picsum.photos/1920/400' alt="Slide 2" />
-                        <Carousel.Caption>
-                            <h3>Slide 2</h3>
-                        </Carousel.Caption>
+                        <img src={bannerImage} alt="Slide 2" />
                     </Carousel.Item>
                     <Carousel.Item>
-                        <img src='https://picsum.photos/1920/400' alt="Slide 3" />
-                        <Carousel.Caption>
-                            <h3>Slide 3</h3>
-                        </Carousel.Caption>
+                        <img src={bannerImage} alt="Slide 3" />
                     </Carousel.Item>
                 </Carousel>
             </div>
+            <div>
+                <h2 className={styles.title}>The Lone Creek Legacy</h2>
+                <p className={styles['home-story']}>In the year 1980, in the heart of Texas, a state known for its vastness and distinctive traditions, Sheriff Robert Thornton decided it was time to invest in the future of his community. Inspired by his passion for education and a desire to provide opportunities for local youth, he founded the Lone Creek Academy. <br />
+
+                <br />Situated in the town center, Lone Creek Academy began modestly, with just a few classrooms and a handful of dedicated teachers. Sheriff Thornton played an active role in overseeing the school, ensuring that community values and the spirit of Texas were embedded in every aspect of the educational environment.<br />
+
+                <br />The early years of Lone Creek Academy were challenging, but Sheriff Thornton's determination and the support of the community kept the flame alive. The school expanded, adding new buildings and broadening its curriculum to meet the evolving needs of the students.<br />
+
+                <br />Over time, Lone Creek Academy became more than an educational institution. It was a place where students not only learned academic subjects but also imbibed fundamental values of respect, responsibility, and pride in Texan heritage. Community spirit flourished within the school, reflecting the mindset of a town that came together to support the next generation.<br />
+
+                <br />Sheriff Thornton, affectionately known as "Sheriff Bob" by the students, continued to play an active role in Lone Creek Academy over the years. His initial vision of providing quality educational opportunities became a reality, and the school became a cornerstone in the Lone Creek community.<br />
+
+                <br />Today, Lone Creek Academy celebrates decades of success, shaping not only well-educated students but citizens committed to the values that have shaped Texas history. Sheriff Thornton's legacy lives on not only within the school's walls but in the hearts of those who have walked its corridors, carrying with them the pride of being products of Lone Creek Academy.</p>
+            </div>
             <footer className={styles.footer}>
-                
             </footer>
         </>
     )
